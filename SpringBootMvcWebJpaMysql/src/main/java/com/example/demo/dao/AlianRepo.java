793d94d5bd68d84a63d0ac31a979b0ef8c91eb03
Abinaya <abiselvam080@gmail.com>
2022-05-26 11:51:06 +0530

    commit 79

---
+++ SpringBootMvcWebJpaMysql/src/main/java/com/example/demo/dao/AlianRepo.java
@@ -0,0 +1,13 @@
+package com.example.demo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Alian;
+@Repository
+public interface AlianRepo extends JpaRepository<Alian, Integer>{
+
+	            //get all CRUD operation
+	
+}
+
